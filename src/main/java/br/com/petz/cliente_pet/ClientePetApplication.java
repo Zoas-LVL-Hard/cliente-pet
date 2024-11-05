48fdf923960db196c983c1592388aad70eec0163
Victor <victormota_03@hotmail.com>
2024-11-05 17:26:07 -0300

    Alterado o construtor de teste da aplicacao

--- src/main/java/br/com/petz/cliente_pet/ClientePetApplication.java
+++ src/main/java/br/com/petz/cliente_pet/ClientePetApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 @RestController
 @RequestMapping("/")
 public class ClientePetApplication {
-
+	@GetMapping
 	public String getHomeTeste() {
 		return "Cliente Pet - API Home";
 	}
