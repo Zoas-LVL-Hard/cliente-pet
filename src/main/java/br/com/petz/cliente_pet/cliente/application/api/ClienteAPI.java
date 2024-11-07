53341e36a701b16795a910194aa9825ca5503bab
Victor <victormota_03@hotmail.com>
2024-11-07 16:54:13 -0300

    Implementa contrato da API

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
@@ -0,0 +1,17 @@
+package br.com.petz.cliente_pet.cliente.application.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/vi/cliente")
+public interface ClienteAPI {
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
+}
