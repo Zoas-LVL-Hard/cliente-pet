bf5d986eeb267af1abb99ece77908289eee829b0
Victor <victormota_03@hotmail.com>
2024-11-13 16:22:12 -0300

    Modifica Cliente para nao permitir email e cpf duplicado

--- src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
@@ -38,6 +38,7 @@ public class Cliente {
     private String nomeCompleto;
     @NotBlank
     @Email
+    @Column (unique = true)
     private String email;
     @NotBlank
     private String celular;
@@ -46,6 +47,7 @@ public class Cliente {
     @NotNull
     private LocalDate dataDeNasc;
     @CPF
+    @Column (unique = true)
     private String cpf;
     @NotNull
     private boolean aceitaTermos;
